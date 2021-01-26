877ca4645c564794e2d579e8ee3ceea6e8a77818
Wesley Costa <wesferrcosta@gmail.com>
2021-01-26 14:20:26 -0300

    Implementado validação IMC

--- imc/script.js
+++ imc/script.js
@@ -6,7 +6,34 @@ function calcularIMC() {
     const peso = document.getElementById('peso').value
     const resultado = document.getElementById('resultado')
 
-    resultado.textContent = `Nome: ${nome} Altura: ${altura} Peso: ${peso}`
+    if (nome !== '' && altura !== '' && peso !== '') {
+        const valor = (peso / (altura * altura)).toFixed(1);
+
+        let classificacao = ''
+        let obesidade = ''
+
+        if (valor < 18.5) {
+            classificacao = 'MAGREZA'
+            obesidade = '0'
+        } else if (valor >= 18.5 && valor <= 24.9) {
+            classificacao = 'NORMAL'
+            obesidade = '0'
+        } else if (valor >= 25 && valor <= 29.9) {
+            classificacao = 'SOBREPESO'
+            obesidade = 'I'
+        } else if (valor >= 30 && valor < 39.9) {
+            classificacao = 'OBESIDADE'
+            obesidade = 'II'
+        } else {
+            classificacao = 'OBESIDADE GRAVE'
+            obesidade = 'III'
+        }
+
+        resultado.textContent = `IMC: ${valor} Classificação: ${classificacao} Obesidade: ${obesidade}`
+    } else {
+        resultado.textContent = `Preencha todos os campos!!!`
+    }
+
 }
 
 calcular.addEventListener('click', calcularIMC);
